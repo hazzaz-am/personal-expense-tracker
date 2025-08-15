343dbc401f2ec2985f24f8d5b2660da3c862529f
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-08-15 21:26:35 +0600

    feat: add cors configuration

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,13 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 
 // routes
 app.use("/api/v1", router);
